64ebb55d48a3f247be0697883312bb19418900f1
hayatSalawdeh <salawdeh.hayat@gmail.com>
2022-09-27 01:53:05 +0300

    done

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
   //add static methods here
+  static capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str){
+    return str.replace(/[^a-zA-Z\'\-\s]/g,"");
+  }
+
+  static titleize(str){
+    const stringList = str.split(' ');
+    const specialChar = ['the','a','an','but','of','and','for','at','by','from'];
+    let result = "";
+    console.log(this.specialChar);
+    stringList.forEach(x=>{
+      if(specialChar.find(element => element === x) !== undefined){
+        result += x + ' ';
+        console.log(x);
+        return;
+      }
+        
+      result +=  this.capitalize(x) + ' ';
+    })
+    console.log("'"+result.trim()+"'");
+   return this.capitalize(result.trim());
+  }
 }
+
+console.log(Formatter.titleize("a tale of two cities"));
